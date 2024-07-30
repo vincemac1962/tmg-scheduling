e48ead155d35f5f22ebe479338b78ea1590fe409
vincemac1962 <vince.macrae@gmail.com>
2024-07-30 10:06:25 +0100

    Extend schedule handling to associate sites with schedules plus supporting views. Extend site_schedule pivot table to log download activity.

--- resources/views/schedules/associate_sites.blade.php
+++ resources/views/schedules/associate_sites.blade.php
@@ -68,7 +68,6 @@
                     <a href="/schedules/{{ $schedule_id }}" class="w-40 bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded mr-2 text-center text-xs">Back</a>
                     <button type="submit" class="w-40 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded mr-2 text-center text-xs">Associate Sites</button>
                 </div>
-                <div
             </form>
                     <div class="flex justify-center mt-4">
                         {{ $sites->links() }}
